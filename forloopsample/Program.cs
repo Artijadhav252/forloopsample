db7576d65050d1b5dd028613ca160b75220f543f
artir <artir@REYANSH2021>
2023-03-20 04:31:01 +0530

    Add project files.

---
+++ forloopsample/Program.cs
@@ -0,0 +1,49 @@
+﻿public class program
+{
+    public static void Main()
+    {
+        firstNestedloop();
+        secondnestedloopo();
+        thirdnestedloop();
+    }
+    public static void firstNestedloop()
+    {
+        for(int i=1; i<=5; i++)
+       
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write(" * ");
+            }
+            Console.WriteLine();
+        }
+       
+    }
+
+    public static void secondnestedloopo()
+    {
+        Console.WriteLine("-----------------------------------------------------------");
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=10; j++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void thirdnestedloop()
+    {
+        Console.WriteLine("----------------------------------------------------");
+        for (int i = 5; i >= 1; i++)
+        {
+            for (int j = 5; j >= 1; j++)
+            {
+                Console.Write(" * ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+}
